386b566485f622d7ee63a67247bae2aa5c2c0f75
rufino <rufi.stg@gmail.com>
2022-02-19 13:38:32 -0600

    completed seed and migration to category and products

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'sku' => $this->faker->bothify('?###??##'),
+            'name' => $this->faker->unique()->sentence(3),
+            'price' => $this->faker->randomFloat(2, 1, 100),
+            'amount' => $this->faker->randomNumber(),
+            'status' => $this->faker->boolean(),
+            'category_id' => $this->faker->numberBetween(1,3) //factory(App\Models\Category::class),
+        ];
+    }
+}
---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function it_visit_page_of_login()
+    {
+        $this->get('/login')
+            ->assertStatus(200)
+            ->assertSee('Login');
+    }
+    /** @test */
+    public function authenticated_to_a_user()
+    {
+        $user = create('App\User', [
+            "email" => "user@mail.com"
+        ]);
+
+        $this->get('/login')->assertSee('Login');
+        $credentials = [
+            "email" => "user@mail.com",
+            "password" => "secret"
+        ];
+
+        $response = $this->post('/login', $credentials);
+        $response->assertRedirect('/home');
+        $this->assertCredentials($credentials);
+    }
+
+    /** @test */
+    public function not_authenticate_to_a_user_with_credentials_invalid()
+    {
+        $user = create('App\User', [
+            "email" => "user@mail.com"
+        ]);
+        $credentials = [
+            "email" => "users@mail.com",
+            "password" => "secret"
+        ];
+
+        $this->assertInvalidCredentials($credentials);
+    }
+
+    /** @test */
+    public function the_email_is_required_for_authenticate()
+    {
+        $user = create('App\User');
+        $credentials = [
+            "email" => null,
+            "password" => "secret"
+        ];
+
+        $response = $this->from('/login')->post('/login', $credentials);
+        $response->assertRedirect('/login')->assertSessionHasErrors([
+            'email' => 'The email field is required.',
+        ]);
+    }
+
+    /** @test */
+    public function the_password_is_required_for_authenticate()
+    {
+        $user = create('App\User', ['email' => 'zaratedev@gmail.com']);
+        $credentials = [
+            "email" => "zaratedev@gmail.com",
+            "password" => null
+        ];
+
+        $response = $this->from('/login')->post('/login', $credentials);
+        $response->assertRedirect('/login')
+            ->assertSessionHasErrors([
+                'password' => 'The password field is required.',
+            ]);
+    }
+}
