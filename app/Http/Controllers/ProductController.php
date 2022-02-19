335e712ab41f1458125ad4fd5ce43176cd048af4
rufino <rufi.stg@gmail.com>
2022-02-19 13:52:42 -0600

    add route and funtionality controller product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::get();
     }
 
     /**
@@ -25,7 +25,8 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $articulo = new Product;
+        $articulo->create($request->all());
     }
 
     /**
@@ -36,7 +37,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return $product;
     }
 
     /**
@@ -48,7 +49,7 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
     }
 
     /**
@@ -59,6 +60,6 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         \App\Models\Category::factory(3)->create();
-        \App\Models\Product::factory(3)->create();
+        \App\Models\Product::factory(200)->create();
     }
 }
